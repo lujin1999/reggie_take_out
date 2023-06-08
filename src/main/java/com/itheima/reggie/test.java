e25a69ba06c142c7b6db06a7ab65b5a39950d628
1443546642@qq.com <1443546642@qq.com>
2023-06-08 20:33:27 +0800

    win_addtest.java

---
+++ src/main/java/com/itheima/reggie/test.java
@@ -0,0 +1,10 @@
+package com.itheima.reggie;
+
+public class test {
+    public static void main(String[] args) {
+        int a =1;
+        int b = 2;
+        int c = 3;
+        System.out.println("123");
+    }
+}
